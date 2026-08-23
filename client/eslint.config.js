c8665abc9785a18e4b3e3109d0b4bab2f78fdf20
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-24 01:04:36 +0300

    lint: ignore generated production output

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,2 +1,8 @@
 import globals from 'globals';
-export default [{files:['**/*.{js,jsx}'],languageOptions:{globals:{...globals.browser,...globals.node},parserOptions:{ecmaVersion:'latest',sourceType:'module',ecmaFeatures:{jsx:true}}},rules:{'no-unused-vars':['warn',{argsIgnorePattern:'^_'}]}}];
+
+export default [{
+  ignores:['dist/**','node_modules/**'],
+  files:['**/*.{js,jsx}'],
+  languageOptions:{globals:{...globals.browser,...globals.node},parserOptions:{ecmaVersion:'latest',sourceType:'module',ecmaFeatures:{jsx:true}}},
+  rules:{'no-unused-vars':['warn',{argsIgnorePattern:'^_'}]}
+}];
